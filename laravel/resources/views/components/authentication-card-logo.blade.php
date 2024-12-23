40030de39636fa90e293ac49a6c549be7d7ab990
Jimmie Johansson <jimmitjoo@gmail.com>
2024-12-24 00:27:12 +0100

    Redesign frontpage and add simple temporary logo

--- laravel/resources/views/components/authentication-card-logo.blade.php
+++ laravel/resources/views/components/authentication-card-logo.blade.php
@@ -1,6 +1,13 @@
 <a href="/">
-    <svg class="size-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-        <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+    <svg class="size-16" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <!-- Huvudcirkel (bollen) -->
+        <circle cx="50" cy="50" r="45" fill="white" stroke="black" stroke-width="2" />
+
+        <!-- Central femkant -->
+        <polygon points="50,20 65,35 58,55 42,55 35,35" fill="black" />
+
+        <!-- Två mindre femkanter på sidorna -->
+        <polygon points="80,45 90,55 85,65 70,60 73,48" fill="black" />
+        <polygon points="20,45 10,55 15,65 30,60 27,48" fill="black" />
     </svg>
 </a>
